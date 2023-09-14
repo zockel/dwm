45b6bc159ea96ffba8d3a423f5ba47ae6075867d
Markus Hanetzok <markus@hanetzok.net>
2023-09-14 12:46:49 +0200

    Revert "apply centeredmaster patch"
    This reverts commit caee0faa03262a18ca8da434ee9b54b2591aec72.

--- config.def.h
+++ config.def.h
@@ -69,8 +69,6 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -105,8 +103,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-	/* { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} }, */
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -117,9 +113,9 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_Tab,    viewnext,       {0} },
 	{ MODKEY|ShiftMask,             XK_Tab,    viewprev,       {0} },
-        /* { MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} }, */
-        /* { MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} }, */
-	{ MODKEY,            		XK_n,      togglescratch,  {.ui = 0 } },
+/* +	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} }, */
+/* +	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} }, */
+	{ MODKEY,            		XK_t,      togglescratch,  {.ui = 0 } },
 	{ MODKEY,            		XK_r,      togglescratch,  {.ui = 1 } },
 	{ MODKEY,            		XK_grave,  togglescratch,  {.ui = 2 } },
 	TAGKEYS(                        XK_1,                      0)
